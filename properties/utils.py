b32f89794097c3d994139fea4c72f38c704bc2ef
Enoch Baah <ebaah2942@gmail.com>
2025-08-29 16:40:23 +0000

    Added caching using redis

---
+++ properties/utils.py
@@ -0,0 +1,14 @@
+from django.core.cache import cache
+from .models import Property
+
+def get_all_properties():
+    # Try fetching from Redis
+    properties = cache.get("all_properties")
+
+    if not properties:
+        # Fetch from DB if not in cache
+        properties = list(Property.objects.values())
+        # Store in Redis for 1 hour (3600 seconds)
+        cache.set("all_properties", properties, 3600)
+
+    return properties
--- properties/views.py
+++ properties/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property 
+from django.views.decorators.csrf import csrf_exempt
+from .utils import get_all_properties
 
 # Create your views here.
-@cache_page(60 * 15)  # Cache the view for 15 minutes 
+@csrf_exempt
 def property_list(request):
-    properties = list(Property.objects.values())
+    properties = get_all_properties()
     return JsonResponse({
         "data": properties
     })
+
